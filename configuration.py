7a6e47ec5b011c0b55c4ea8ddc655c5c5e718750
Olya <blednayao@gmail.com>
2025-02-05 11:15:45 +0200

    post_order

---
+++ configuration.py
@@ -0,0 +1,8 @@
+#базовый URL сервиса
+URL_SERVICE = "https://9f1caadc-e25f-4b1a-be23-e19c6ddc3544.serverhub.praktikum-services.ru"
+
+#путь к документации
+DOC_PATH = "/docs/" 
+
+#пусть создание заказа
+ORDER_PATH = "/api/v1/orders"
---
+++ sender_stand_request.py
@@ -0,0 +1,16 @@
+import configuration
+import requests
+import data
+
+#функция создания нового заказа
+def post_order():
+    #выполнение пост запроса
+    return requests.post(url=configuration.URL_SERVICE + configuration.ORDER_PATH,
+                         json=data.body)
+
+response = post_order()
+
+# Вывод статус-кода
+print("Status Code:", response.status_code)
+# Вывод тела ответа в виде текста
+print("Response Body:", response.text)
