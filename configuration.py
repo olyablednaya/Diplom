f62e523d59456a65e78819abdeeb7a562d4f1e73
Olya <blednayao@gmail.com>
2025-02-05 12:19:13 +0200

    adding_get_order_by_number

--- configuration.py
+++ configuration.py
@@ -1,8 +1,11 @@
 #базовый URL сервиса
-URL_SERVICE = "https://9f1caadc-e25f-4b1a-be23-e19c6ddc3544.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://e0afd323-cf85-4cce-bbb3-9b780a43ce66.serverhub.praktikum-services.ru"
 
 #путь к документации
 DOC_PATH = "/docs/" 
 
 #пусть создание заказа
 ORDER_PATH = "/api/v1/orders"
+
+#путь получения заказа по его номеру
+GET_ORDER_NUMBER_PATH = "/api/v1/orders/track?t="
--- sender_stand_request.py
+++ sender_stand_request.py
@@ -7,7 +7,6 @@ def post_order():
     #выполнение пост запроса
     return requests.post(url=configuration.URL_SERVICE + configuration.ORDER_PATH,
                          json=data.body)
-                         
 
 response = post_order()
 
@@ -16,8 +15,14 @@ print("Status Code:", response.status_code)
 # Вывод тела ответа в виде текста
 print("Response Body:", response.text)
 
-
+#сохраняем трек номер
 track_number = response.json()["track"]
 print(track_number)
 
+def get_order_by_number(track_number):
+    #выполенение гет запроса
+    return requests.get(url=configuration.URL_SERVICE + configuration.GET_ORDER_NUMBER_PATH + str(track_number))
 
+response_get_order = get_order_by_number(track_number)
+print(response_get_order.status_code)
+print(response_get_order.text)
